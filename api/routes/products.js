1ebd22abae9a9a5dd322488f3c1f3492f789eb88
RaviChandharpb <ravifabulous471@gmail.com>
2018-03-06 12:40:59 +0530

    hello

---
+++ api/routes/products.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const router = express.Router();
+const multer = require('multer');
+const checkAuth = require('../middleware/check-auth');
+const ProductsController = require('../controllers/products');
+const path = require('path');
+/*
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, '');
+  },
+  filename: function(req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  // reject a file
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'|| file.mimetype === 'image/jpg') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+});
+*/
+// Set The Storage Engine
+const storage = multer.diskStorage({
+  destination: './public/uploads/',
+  filename: function(req, file, cb){
+    cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+// Init Upload
+const upload = multer({
+  storage: storage,
+  limits:{fileSize: 1000000},
+  fileFilter: function(req, file, cb){
+    checkFileType(file, cb);
+  }
+});
+
+function checkFileType(file, cb){
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|gif/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if(mimetype && extname){
+    return cb(null,true);
+  } else {
+    cb('Error: Images Only!');
+  }
+}
+
+router.get("/", ProductsController.products_get_all);
+
+//router.post("/", checkAuth,  ProductsController.products_create_product);
+
+router.post("/",checkAuth, upload.single('productImage'), ProductsController.products_create_product);
+
+router.get("/:productId",checkAuth, ProductsController.products_get_product);
+
+router.patch("/:productId", checkAuth, ProductsController.products_update_product);
+
+router.delete("/:productId", checkAuth, ProductsController.products_delete);
+
+module.exports = router;
